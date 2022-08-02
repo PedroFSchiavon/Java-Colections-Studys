524040ca09189968e82d93c76fd4ff2ef515c30b
Pedro F Schiavon <pedrofschiavon@gmail.com>
2022-08-02 16:27:11 -0300

    Usando referencias de metodos.

--- lambda-e-streams/src/br/com/alura/testes/TesteLambda.java
+++ lambda-e-streams/src/br/com/alura/testes/TesteLambda.java
@@ -1,6 +1,7 @@
 package br.com.alura.testes;
 
 import java.util.ArrayList;
+import static java.util.Comparator.comparing;
 import java.util.List;
 
 public class TesteLambda {
@@ -12,8 +13,11 @@ public class TesteLambda {
         palavras.add("O que ninguem te contou, é que a broa na verdade é um bebé");
         palavras.add("Hahahaha");
 
-        palavras.forEach(palavra -> System.out.println(palavra));
-        palavras.sort((p1, p2) -> Integer.compare(p1.length(), p2.length()));
+        palavras.forEach(System.out::println);
+//        palavras.sort((p1, p2) -> Integer.compare(p1.length(), p2.length()));
+        //palavras.sort(comparing(String::length));
+        palavras.sort(String::compareTo);
+        System.out.println("--------------------------------------------");
         palavras.forEach(palavra -> System.out.println(palavra));
     }
 }
